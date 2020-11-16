a1a33cb91fe93b46e0d7dbe3632350b3f1f1d0c0
Илья Долженко <ilay00@mail.ru>
2020-11-16 21:51:59 +0300

    init

---
+++ app/js/navbar-scroll.js
@@ -0,0 +1,26 @@
+const navbar = document.getElementById("navbar");
+
+let timerId;
+
+const throttle = function (func, delay) {
+    if (timerId) {
+        return;
+    }
+    timerId = setTimeout(function () {
+        func();
+        timerId = undefined;
+    }, delay);
+};
+
+function onScroll() {
+    if (window.scrollY >= 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+    console.log("render");
+}
+
+document.addEventListener("scroll", function () {
+    throttle(onScroll, 100);
+});
